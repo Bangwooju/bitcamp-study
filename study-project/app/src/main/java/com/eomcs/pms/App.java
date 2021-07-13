adb8bf5e08f7c603b2073eb4c9fcc2f030684867
Bangwooju <phonestudio97@gmail.com>
2021-07-13 18:55:28 +0900

    Day12

--- study-project/app/src/main/java/com/eomcs/pms/App.java
+++ study-project/app/src/main/java/com/eomcs/pms/App.java
@@ -2,34 +2,40 @@ package com.eomcs.pms;
 
 import java.sql.Date;
 import java.util.Scanner;
+//1) 회원 데이터를 입력 받는 코드를 별도의 메서드로 분리한다.
+//=> addMember() 메서드 정의 및 회원 등록 코드를 가져오기
+//=> main()과 addMember()가 서로 공유하는 변수는 클래스 변수로 만든다.
+//2) 입력된 회원 데이터의 목록을 출력하는 코드를 별도의 메서드로 분리한다.
+//=> listMembers() 메서드 정의 및 관련된 코드를 가져오기
+//3) 프로젝트 데이터를 입력 받는 코드를 별도의 메서드로 분리한다.
+//=> addProject() 메서드 정의 및 관련 코드를 가져오기
+//=> main()과 addProject()가 서로 공유하는 변수는 클래스 변수로 만든다.
+//4) 입력된 프로젝트 데이터의 목록을 출력하는 코드를 별도의 메서드로 분리한다.
+//=> listProjects() 메서드 정의 및 관련된 코드를 가져오기
+//5) 작업 데이터를 입력 받는 코드를 별도의 메서드로 분리한다.
+//=> addTask() 메서드 정의 및 관련 코드를 가져오기
+//=> main()과 addTask()가 서로 공유하는 변수는 클래스 변수로 만든다.
+//6) 입력된 작업 데이터의 목록을 출력하는 코드를 별도의 메서드로 분리한다.
+//=> listTasks() 메서드 정의 및 관련된 코드를 가져오기
+//7) 사용자로부터 명령어를 입력 받는 코드를 별도의 메서드로 분리한다.
+//=> prompt() 메서드를 정의하고 관련된 코드를 가져오기\
+//8) 명령어 뿐만 아니라 사용자로부터 입력을 받는 기능을 모두 prompt()를 사용하여 처리한다.
+//=> prompt()를 호출할 때 사용자에게 출력할 프롬프트 메시지를 아규먼트로 넘긴다.
+//=> prompt()는 호출할 때 넘어온 프롬프트 메시지 값을 파라미터로 받는다.
+//9) prompt() 가 작업한 결과를 클래스 변수에 담지 말고 호출자에게 리턴한다.
+//=> prompt()를 실행한 후 사용자가 입력한 문자열을 리턴한다.
+//10) prompt()의 기능을 더 세분화한다.
+//=> promptString() : 사용자로부터 문자열을 입력 받아 리턴한다.
+//=> promptInt() : 사용자로부터 숫자를 입력 받아 리턴한다.
+//=> promptDate() : 사용자로부터 날짜를 입력 받아 리턴한다.
+//11) promtString(), promptInt(), promptDate() 메서드의 코드를 정리한다.
+
 
 public class App {
-  // 1) addMember로 회원정보 입력을 분리한다. 매서드 정의 및 회원등록 코드 가져오기
-  //=>main()과 addMember() 가 서로 공유하는 변수는 클래스 변수로 만든다.
-  // 2) 입력된 회원 데이터의 목록을 출력하는 코드를 별도의 메서드로 분리
-  //=>listMembers()메서드 정의 및 관련된 코드 가져오기
-  //3) 프로젝트 add하기
-  //4) 프로젝트 list만들기 
-  //5) 작업 add 만들기
-  //6) 작업 list 만들기
-  //7) 사용자로부터 명령어를 입력받는 코드를 별도의 메서드로 분리
-  //=> 즉각적으로 다음 명령을 묻는 것을 prompt라고 한다.
-  // prompt() 메서드 정의하고 코드 가져오기
-  //8) 명령어 뿐만 아니라 사용자로부터 입력을 받는 기능을 prompt()로 처리
-  // => prompt를 호출할 때 사용자에게 출력할 프롬프트 메시지를 설정하도록 변경
-  //9) prompt()를 호출할 때 사용자에게 출력할 프롬프트 메시지를 아규먼트로 넘긴다. 
-  //prompt()는 호출할 때 넘어온 프롬프트 메시지값을 파라미터로 받는다.
-  //prompt()가 작업한 결과를 클래스 변수에 담지 말고 호출자에게 리턴한다.
-  //10)prompt() 의 기능을 더 세분화한다.
-  //promptString() : 사용자로부터 문자열을 입력받아 리턴
-  //promptInt() : 사용자로부터 정수를 입력받아 리턴
-  //promptDate() : 사용자로부터 날짜을 입력받아 리턴
-  //11) promptString(), promptInt(), promptDate()에 중복값을 제거한다
-
   static Scanner keyboardScan = new Scanner(System.in);
 
-  //회원정보
-  static  final int LENGTH = 100;
+  // 회원 정보
+  static final int LENGTH = 100;
   static int[] no = new int[LENGTH];
   static String[] name = new String[LENGTH];
   static String[] email = new String[LENGTH];
@@ -39,9 +45,8 @@ public class App {
   static Date[] registeredDate = new Date[LENGTH];
   static int size = 0;
 
-  //프로젝트 정보
+  // 프로젝트 정보
   static final int PROJECT_LENGTH = 1000;
-
   static int[] pNo = new int[PROJECT_LENGTH];
   static String[] pTitle = new String[PROJECT_LENGTH];
   static String[] pContent = new String[PROJECT_LENGTH];
@@ -51,7 +56,7 @@ public class App {
   static String[] pMembers = new String[PROJECT_LENGTH];
   static int pSize = 0;
 
-  //업무 정보
+  // 작업 정보
   static final int TASK_LENGTH = 100;
   static int[] tNo = new int[TASK_LENGTH];
   static String[] tContent = new String[TASK_LENGTH];
@@ -62,52 +67,51 @@ public class App {
 
   public static void main(String[] args) {
 
-    while (true) { 
+    while (true) {
       String input = promptString("명령> ");
 
-      if (input.equals("quit")|| input.equals("exit")) {
+      if (input.equals("exit") || input.equals("quit")) {
         System.out.println("안녕!");
         break;
       } else if (input.equals("/member/add")) {
-        //메서드로 분리한 코드를 실행하기 (메서드 호출)
-        // 메서드 명();
         addMember();
 
       } else if (input.equals("/member/list")) {
         listMembers();
 
-      } else if (input.equals("/project/add")) {
+      }  else if (input.equals("/project/add")) {
         addProject();
 
-      }else if (input.equals("/project/list")) {
+      }  else if (input.equals("/project/list")) {
         listProjects();
 
-      } else if (input.equals("/task/add")) {
+      }  else if (input.equals("/task/add")) {
         addTask();
 
-      } else if (input.equals("/task/list")){
+      }  else if (input.equals("/task/list")) {
         listTasks();
-      }
-      else {
+
+      } else {
         System.out.println("실행할 수 없는 명령입니다.");
       }
       System.out.println();
     }
     keyboardScan.close();
   }
-
   static void addMember() {
     System.out.println("[회원 등록]");
 
     no[size] = promptInt("번호? ");
-    name[size] = promptString("이름? ");
-    email[size] = promptString("메일? ");
+    name[size] = promptString("이름? ");;
+    email[size] = promptString("이메일? ");
     password[size] = promptString("암호? ");
     photo[size] = promptString("사진? ");
     tel[size] = promptString("전화? ");
+
     registeredDate[size] = new Date(System.currentTimeMillis());
 
     size++;
+
   }
 
   static void listMembers() {
@@ -124,7 +128,7 @@ public class App {
     System.out.println("[프로젝트 등록]");
 
     pNo[pSize] = promptInt("번호? ");
-    pTitle[pSize] = promptString("프로젝트명? ");
+    pTitle[pSize] = promptString("프로젝트명?> ");
     pContent[pSize] = promptString("내용? ");
     pStartDate[pSize] = promptDate("시작일? ");
     pEndDate[pSize] = promptDate("종료일? ");
@@ -132,7 +136,6 @@ public class App {
     pMembers[pSize] = promptString("팀원? ");
 
     pSize++;
-
   }
 
   static void listProjects() {
@@ -141,7 +144,6 @@ public class App {
       // 번호, 프로젝트명, 시작일, 종료일, 만든이
       System.out.printf("%d, %s, %s, %s, %s\n", // 출력 형식 지정
           pNo[i], pTitle[i], pStartDate[i], pEndDate[i], pOwner[i]);
-
     }
   }
 
@@ -149,23 +151,20 @@ public class App {
     System.out.println("[작업 등록]");
 
     tNo[tSize] = promptInt("번호? ");
-    tContent[tSize] = promptString("내용? ");
+    tContent[tSize] =promptString("내용? ");
     tDeadline[tSize] = promptDate("마감일? ");
 
     System.out.println("상태?");
     System.out.println("0: 신규");
     System.out.println("1: 진행중");
     System.out.println("2: 완료");
-
-    tStatus[tSize] = promptInt(">" );
+    tStatus[tSize] = promptInt("> ");
     tOwner[tSize] = promptString("담당자? ");
 
     tSize++;
-
   }
 
-  static void listTasks() {
-
+  static void listTasks() {  
     System.out.println("[작업 목록]");
     for (int i = 0; i < tSize; i++) {
       String stateLabel = null;
@@ -179,35 +178,29 @@ public class App {
         default:
           stateLabel = "신규";
       }
+      // 번호, 작업명, 마감일, 프로젝트, 상태, 담당자
       System.out.printf("%d, %s, %s, %s, %s\n", // 출력 형식 지정
           tNo[i], tContent[i], tDeadline[i], stateLabel, tOwner[i]);
     }
-
-
   }
 
-  //prompt();
-  // - 사용자로부터 문자열을 입력 받는 기능을 수행한다.
-  // - 파라미터:
-  //     title - 프롬프트의 메시지
-  // - 리턴 값:
-  //     String - 사용자가 입력한 문자열
-
-
-  // 사용자로부터 문자열을 입력받아 리턴한다
   static String promptString(String title) {
     System.out.print(title);
     return keyboardScan.nextLine();
   }
 
-  // 사용자로부터 숫자를 입력받아 리턴한다
   static int promptInt(String title) {
-    return Integer.parseInt(promptString(title));
+    System.out.print(title);
+    return Integer.parseInt(promptString (title));
+
   }
 
-  // 사용자로부터 날짜열을 입력받아 리턴한다.
   static Date promptDate(String title) {
+    System.out.print(title);
     return Date.valueOf(promptString(title));
+
   }
+
+
 
 }
