8020ddca4d777bfda7c3aaf0626514b4a1585248
Bangwooju <phonestudio97@gmail.com>
2021-08-03 20:49:14 +0900

    Day27

--- study-project/app/src/main/java/com/eomcs/pms/menu/MenuGroup.java
+++ study-project/app/src/main/java/com/eomcs/pms/menu/MenuGroup.java
@@ -2,107 +2,82 @@ package com.eomcs.pms.menu;
 
 import com.eomcs.util.Prompt;
 
-public class MenuGroup extends Menu {
-  // 역할
-  //- 다른 메뉴를 포함하는 컨테이너 역할을 수행한다.
-  Menu[] childs = new Menu[100]; // Leaf 와 함께 다른 메뉴를 포함할 수 있다. 
-  int size ;
-  boolean disablePrevMenu;
-  String prevMenuTitle = "이전 메뉴";
+public class MenuGroup extends Menu{
 
-  // 생성자를 정의하지 않으면 컴파일러가 기본 생성자를 자동으로 추가해준다.
-  // 문제는 컴파일러가 추가한 기본 생성자는 수퍼클래스의 기본 생성자를 호출하는데
-  // Menu에 기본 생성자가 없기 때문에 컴파일 오류가 발생한다.
-  // 따라서 개발자가 직접 생성자를 정의해야한다.
+  Menu[] childs = new Menu[100];
+  int size;
+  String prevMenuTitle = "이전 메뉴";
+  boolean disablePreMenu;
 
   public MenuGroup(String title) {
     super(title);
   }
 
-  public MenuGroup(String title, boolean disablePrevMenu) {
-    super(title);
-    this.disablePrevMenu = disablePrevMenu;
-  }
-
-
-  public void setPrevMenuTitle(String prevMenuTitle) {
-    this.prevMenuTitle = prevMenuTitle;
-  }
-
-  //MenuGroup이 포함하는 하위 Menu를 다룰 수 있도록 메서드를 정의한다.
-
   public void add(Menu child) {
-    if(this.size == this.childs.length) {
-      return; // 하위 메뉴를 저장할 배열이 꽉 찼다면 더이상 저장해서는 안된다.
+    if(this.childs.length == this.size) {
+      return;
     }
-
-    this.childs[this.size++] = child;
+    this.childs[this.size++] = child; 
   }
 
-  // 배열에 들어있는 Menu 객체를 찾아 제거한다.
+
   public Menu remove(Menu child) {
     int index = indexOf(child);
     if(index == -1) {
       return null;
     }
-    for(int i = index+1 ; i <this.size ; i++) {
+    for(int i = index + 1; i < this.size; i++) {
       this.childs[i-1] = this.childs[i];
     }
-    childs[--this.size] = null;
+    this.childs[--this.size] = null;
     return child;
   }
 
 
-  // 배열에 들어있는 Menu객체의 인덱스를 알아낸다.
-  public int indexOf (Menu child) {
-    for(int i = 0; i < this.size ; i++) {
-      if(childs[i] == child) {
+  public int indexOf(Menu child) {
+    for(int i = 0; i < this.size; i++) {
+      if(this.childs[i] == child) {
         return i;
       }
     }
     return -1;
   }
 
-
-  // 배열에 들어있는 Menu 객체를 찾는다
   public Menu getMenu(String title) {
-    for(int i = 0; i < this.size ; i++) {
+    for(int i = 0; i < this.size; i++) {
       if(this.childs[i].title.equals(title)) {
-        return this.childs[i];        
+        return this.childs[i];
       }
     }
-    // 완성하시오
     return null;
   }
 
-  @Override // 컴파일러에게 오버라이딩을 제대로 하는지 조사해달라고 요구한다.
+
+  @Override
   public void execute() {
+
     while(true) {
-      System.out.printf("\n[%s]\n", this.title);
-      for(int i =0; i < this.size; i++) {
-        System.out.printf("%d. %s\n", i + 1, this.childs[i].title);
+      System.out.printf("[%s]\n", this.title);
+      for(int i = 0; i < this.size; i++) {
+        System.out.printf("%d, %s\n", i+1, this.childs[i].title);
       }
-
-      if(!disablePrevMenu ) {
-        System.out.printf("0.%s\n" , this.prevMenuTitle);
+      if(!disablePreMenu) {
+        System.out.printf("0. %s\n", this.prevMenuTitle);
       }
-
       int menuNo = Prompt.inputInt("선택> ");
-
-      if(menuNo == 0 && !disablePrevMenu) {
+      if(menuNo == 0 && !disablePreMenu) {
         return;
       }
-      if( menuNo < 0 || menuNo > this.size) {
+      if(menuNo < 0 || menuNo > this.size) {
         System.out.println("무효한 메뉴 번호입니다.");
         continue;
       }
       this.childs[menuNo -1].execute();
-
     }
-
-
-
   }
 
+  public void setPrevMenuTitle(String prevMenuTitle) {
+    this.prevMenuTitle = prevMenuTitle;
+  }
 
 }
