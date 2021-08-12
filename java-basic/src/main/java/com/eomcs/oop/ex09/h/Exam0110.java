26b9c94d909d66a992ae7cb58098125acc6591b0
Bangwooju <phonestudio97@gmail.com>
2021-08-12 18:16:30 +0900

    Day34

--- java-basic/src/main/java/com/eomcs/oop/ex09/h/Exam0110.java
+++ java-basic/src/main/java/com/eomcs/oop/ex09/h/Exam0110.java
@@ -3,7 +3,11 @@ package com.eomcs.oop.ex09.h;
 
 public class Exam0110 {
   public static void main(String[] args) {
-    Tico c1 = new Tico();
+    Tico c1 = new Tico(); // Car객체이면서 CarCheckInfo로 사용할 수 있는 객체이다.
+
+    // Tico가 직접 CarCheckInfo를 구현한 것은 아니지만
+    // Tico의 수퍼클래스가 구현했기 때문에 Tico도 구현한 것으로 간주한다.
+    // Tico 클래스는 수퍼클래스가 구현한 메서드를 상속받기 때문이다.
 
     if (CarCheckInfo.validate(c1)) {
       c1.start();
