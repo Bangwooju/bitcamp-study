bc4aedf728fb6a9a6181c86e54e71d64340f1258
Bangwooju <phonestudio97@gmail.com>
2021-09-13 09:18:00 +0900

    Day54

---
+++ java-basic/src/main/java/com/eomcs/design_pattern/decorator/ex01/after/Printer.java
@@ -0,0 +1,7 @@
+package com.eomcs.design_pattern.decorator.ex01.after;
+
+public abstract class Printer {
+
+  public abstract void f1(String name);
+
+}
---
+++ java-basic/src/main/java/com/eomcs/design_pattern/decorator/ex01/before/C.java
@@ -0,0 +1,15 @@
+package com.eomcs.design_pattern.decorator.ex01.before;
+
+public class C extends B {
+
+  @Override
+  public void f1(String name) {
+    super.f1(name); // 기존 기능 그대로 실행
+
+    // 새 기능을 덧붙인다.
+    // => 꼬릿말을 출력한다.
+
+    System.out.println("************************[끝]");
+  }
+
+}
