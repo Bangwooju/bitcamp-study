5cd4802094093773641901190e5556b1fc59d5e8
Bangwooju <phonestudio97@gmail.com>
2021-09-25 16:43:53 +0900

    Day61

---
+++ study-project/app-client/src/main/java/com/eomcs/pms/ClinetApp.java
@@ -0,0 +1,22 @@
+package com.eomcs.pms;
+
+public class ClinetApp {
+
+
+  public static void main(String[] args) {
+    System.out.println("[PMS 클라이언트]");
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ study-project/app-server/src/main/java/com/eomcs/pms/ServerApp.java
@@ -0,0 +1,23 @@
+package com.eomcs.pms;
+
+public class ServerApp {
+
+
+  public static void main(String[] args) {
+    System.out.println("[PMS 서버]");
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
