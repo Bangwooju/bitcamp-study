f74857625a4498b37f9de2998f0c4d9f7995abe4
Bangwooju <phonestudio97@gmail.com>
2021-12-07 22:21:31 +0900

    ,

--- study-project/app-server/src/main/java/com/eomcs/pms/service/BoardService.java
+++ study-project/app-server/src/main/java/com/eomcs/pms/service/BoardService.java
@@ -4,15 +4,10 @@ import java.util.List;
 import com.eomcs.pms.domain.Board;
 
 public interface BoardService {
-  // 메서드는 업무 용어를 사용한다.
-
-  void add(Board board) throws Exception;
+  int delete(int no) throws Exception;
+  int add(Board board) throws Exception;
   List<Board> list() throws Exception;
-  List<Board> search(String keyword) throws Exception;
+  List<Board> list(String keyword) throws Exception;
   Board get(int no) throws Exception;
-  void update(Board board) throws Exception;
-  void remove(int no) throws Exception;
-
-
-
+  int update(Board board) throws Exception;
 }
